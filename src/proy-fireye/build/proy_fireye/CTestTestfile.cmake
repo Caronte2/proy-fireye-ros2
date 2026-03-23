0d701cad2bcd8c8ba32557c91e0e64fdf4cc6e38
GreysyBurgos <122355310+GreysyBurgos@users.noreply.github.com>
2026-03-23 09:58:24 +0100

    carpetas innecesarias

---
+++ src/proy-fireye/build/proy_fireye/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/grey/Documentos/GitHub/proy-fireye-ros2/proy-fireye
+# Build directory: /home/grey/Documentos/GitHub/proy-fireye-ros2/proy-fireye/build/proy_fireye
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
