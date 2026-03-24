89b891103f3dfb4cb0388c8d9685d28b28a2c3a9
GreysyBurgos <122355310+GreysyBurgos@users.noreply.github.com>
2026-03-24 10:29:35 +0100

    no se hace colcon build en src NUNCA

--- src/build/proy_fireye/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/grey/Documentos/GitHub/proy-fireye-ros2/src/proy-fireye
-# Build directory: /home/grey/Documentos/GitHub/proy-fireye-ros2/src/build/proy_fireye
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
