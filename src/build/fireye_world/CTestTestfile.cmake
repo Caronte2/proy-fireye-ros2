ff9ef198c346a801dc077a8a49030daa28eeba80
GreysyBurgos <122355310+GreysyBurgos@users.noreply.github.com>
2026-03-28 23:03:35 +0100

    Revert "no se hace colcon build en src NUNCA"
    This reverts commit 89b891103f3dfb4cb0388c8d9685d28b28a2c3a9.

---
+++ src/build/fireye_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/grey/Documentos/GitHub/proy-fireye-ros2/src/proy-fireye_world
+# Build directory: /home/grey/Documentos/GitHub/proy-fireye-ros2/src/build/fireye_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
