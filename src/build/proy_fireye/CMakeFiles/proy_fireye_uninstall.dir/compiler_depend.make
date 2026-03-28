056d09986c64e099b4dfda8267724f4aba98b454
GreysyBurgos <122355310+GreysyBurgos@users.noreply.github.com>
2026-03-29 00:33:10 +0100

    mundo con suelo "arreglado"

--- src/build/proy_fireye/CMakeFiles/proy_fireye_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for proy_fireye_uninstall.
-# This may be replaced when dependencies are built.
--- src/proy_fireye_world/launch/fireye_world.launch.py
+++ src/proy_fireye_world/launch/fireye_world.launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             'gz_args': ['-r -s -v2 ', world],
-            'on_exit_shutdown': 'true'
+            'on_exit_shutdown': 'false'
         }.items()
     )
 
@@ -59,15 +59,16 @@ def generate_launch_description():
     set_env_vars_resources = AppendEnvironmentVariable(
         'GZ_SIM_RESOURCE_PATH',
         os.path.join(
-            get_package_share_directory('my_world'),
+            get_package_share_directory('proy_fireye_world'),
             'models'))
 
     ld = LaunchDescription()
 
+    ld.add_action(set_env_vars_resources)
     ld.add_action(gzserver_cmd)
     ld.add_action(gzclient_cmd)
     ld.add_action(spawn_turtlebot_cmd)
     ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(set_env_vars_resources)
+    
 
     return ld
